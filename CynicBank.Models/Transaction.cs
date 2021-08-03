c95cf71818661cb5ec2841af0c2274ba5f90c1be
techtheriac <jezreel.franklin@decagon.dev>
2021-08-03 07:15:50 +0100

    Update transaction model

--- CynicBank.Models/Transaction.cs
+++ CynicBank.Models/Transaction.cs
@@ -10,26 +10,12 @@ namespace Models
         /// Models Bank transaction details
         /// </summary>
         public string Id { get; set; }
-        public DateTimeOffset Date { get; private set; } = DateTimeOffset.Now;
+        public DateTime Date { get; private set; } = DateTime.Now;
 
         public string Description { get; set; }
 
         public int Amount { get; set; }
 
         public TransactionType TypeOfTransaction { get; set; }
-
-        //public Transaction(
-        //    string id,
-        //    int amount,
-        //    string description,
-        //    TransactionType typeOfTransaction
-        //    )
-        //{
-        //    Id = id;
-        //    Amount = amount;
-        //    Description = description;
-        //    TypeOfTransaction = typeOfTransaction;
-        //}
-
     }
 }
