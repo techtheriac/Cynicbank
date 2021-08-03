7e994c8adf3f1a58a0fe8079b140a9fb6369452b
techtheriac <jezreel.franklin@decagon.dev>
2021-08-03 07:16:28 +0100

    Add Session class

---
+++ CynicBank.Persistence/Session.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CynicBank.Persistence
+{
+    public static class Session
+    {
+        public static string ConnectionString { get; set; }
+            = @"Data Source=LAPTOP-0VSJ0RU3;Initial Catalog=CynicDB;Integrated Security=True;Pooling=False";
+
+
+    }
+}
